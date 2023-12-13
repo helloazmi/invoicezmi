2906d26ffa23c79eabef2fd5627b0d9ed1fdd53c
Azz <azmi.akash@gmail.com>
2023-12-13 13:31:54 +0100

    Uploading first draft

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('initated');
+
+
+let dueDate = 2023;
+let sumToPay;
+let reference = 'FAKT'+30;
+let bankAccount = 18492933186;
+let customerNum = 25;
+let customerName = 'Fille';
+
+
+{
+    description: 'Uppdrag1',
+    moms:  25,
+    piece: 1,
+    unitPrice: 1000,
+    totalPay: this.piece*this.unitPrice
+}
